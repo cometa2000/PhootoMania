9dcbcee0e47596e2e268b4ef6fc10f9b7eba3fe4
cometa2000 <tuxmorro2016@gmail.com>
2023-11-25 14:06:14 -0600

    version 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+  }
 }
